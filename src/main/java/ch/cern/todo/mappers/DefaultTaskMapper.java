d2aaff1a67a0dac047cae8d978f64cee4da7ba19
Luca Piras <luca.piras@outlook.com>
2023-09-18 19:17:22 +0100

    Fix formatting

--- src/main/java/ch/cern/todo/mappers/DefaultTaskMapper.java
+++ src/main/java/ch/cern/todo/mappers/DefaultTaskMapper.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultTaskMapper implements TaskMapper {
-    
+
     private final TaskCategoryMapper taskCategoryMapper;
 
     @Autowired
